55cddb68ec7e02dc7a2a8ccb34796736043eaa4a
Enhanced Dom <enhanced.dom@gmail.com>
2022-05-06 18:04:33 +0200

    husky pre-commit installation script

--- src/husky.ts
+++ src/husky.ts
@@ -1,8 +1,26 @@
-export const huskyConfigFactory = (extraHooks = {} as Record<string, string>) => {
-  return {
-    hooks: {
-      'pre-commit': 'lint-staged -r',
-      ...extraHooks
-    }
-  }
+#!/usr/bin/env node
+
+import { add, install } from 'husky'
+import path from 'path'
+import yargs from 'yargs'
+import fs from 'fs'
+import os from 'os'
+
+const args = yargs(process.argv).usage("$0 --folder relative_or_absolute")
+    .default('folder', '.husky').argv as {folder: string}
+
+const fullHuskyHooksPath = path.isAbsolute(args.folder) ? args.folder : path.resolve(process.cwd(), args.folder)
+const fullHuskyPreCommitPath = path.join(fullHuskyHooksPath, 'pre-commit')
+
+if (fs.existsSync(fullHuskyHooksPath)) {
+  fs.rmSync(fullHuskyHooksPath, { recursive: true, force: true })
 }
+
+fs.mkdirSync(fullHuskyHooksPath)
+install(fullHuskyHooksPath)
+add(fullHuskyPreCommitPath,'echo "executing git pre-commit hook via husky"')
+add(fullHuskyPreCommitPath, 'binpath=`npm bin`')
+add(fullHuskyPreCommitPath, 'currentpath=`pwd`')
+add(fullHuskyPreCommitPath, '.${binpath#"$currentpath"}/lint-staged -r')
+const homeDir = os.homedir();
+fs.writeFileSync(path.join(homeDir, '.huskyrc'), `export PATH=${process.env.PATH}`)
